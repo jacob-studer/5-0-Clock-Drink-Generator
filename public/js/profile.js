b6775034a56ec6fa715893ff34e31b6a6543b87a
Jake Studer <jacob.studer.dev@gmail.com>
2021-09-01 22:51:12 -0600

    cleaned some unused code, delete functionality fixed

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,35 +1,8 @@
-// const note = document.querySelector('.note-save')
-// const noteBtn =  document.querySelector('#noteSaveButtonThisIs')
-
-// function saveNoteAPICall(){
-//     let entry = {
-//         value: note.value
-//     }
-//     fetch('/api/notes/new', {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(entry)
-//     })
-//         .then(data => data.json())
-//         .then(res => console.log(res))
-// }
-// function loadNotes(){
-//     fetch('/api/notes')
-//         .then(res => res.json())
-//         .then(data => {
-//             //here lies the array of notes from, per user
-//             console.log(data)
-//         })
-// }
-// loadNotes()
-// noteBtn.addEventListener("click", saveNoteAPICall)
-// //api fetch when the page loads, note.value = res.note (whatever it is saved as in the database)
-
-
-
 //selecting the saved drink and setting it to local storage, document replace to its individual page
+// const drinkTarget = document.querySelector('.drink-name');
+
+// drinkTarget.addEventListener('click', setItemIndividual);
+
 const drinkTarget = document.querySelector('.drink-name');
 
 drinkTarget.addEventListener('click', setItemIndividual);
@@ -41,10 +14,10 @@ function setItemIndividual(event) {
 }
 
 
-const deleteButton = document.querySelector('.delete-button');
 
-deleteButton.addEventListener('click', delButtonHandler);
+// const deleteButton = document.querySelector('.delete-button');
 
+// deleteButton.addEventListener('click', delButtonHandler);
 const delButtonHandler = async (event) => {
     if (event.target.hasAttribute('data-id')) {
         const id = event.target.getAttribute('data-id');
@@ -56,7 +29,14 @@ const delButtonHandler = async (event) => {
         if (response.ok) {
         document.location.replace('/profile');
         } else {
-        alert('Failed to delete project');
+        alert('Failed to delete drink');
         }
     }
     };
+
+const deleteButton = document.querySelector('.delete-button');
+
+deleteButton.addEventListener('click', delButtonHandler);
+
+
+
